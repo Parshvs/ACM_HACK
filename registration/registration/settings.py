38a4a6974febc61491029c3159fb2eff13550409
Parshvs <68771399+Parshvs@users.noreply.github.com>
2023-03-17 09:57:36 +0530

    Update settings.py

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5t1k=7yft%#x301n_n82*m@mks#n#*(#x!2g1z-ab-4yq_1son'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
